ebca4b1d257c9ff864a3aae314e479704a42b0a7
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-02-25 16:39:46 -0500

    Add files via upload

---
+++ plugins/owner-update.js
@@ -0,0 +1,13 @@
+import { execSync } from 'child_process'
+
+let handler = async (m, { conn, text }) => {
+   let stdout = execSync('git pull https://Gatito-kw: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@github.com/Gatito-kw/nekobot-md')
+   await m.reply(stdout.toString())
+}
+
+handler.help = ['update']
+handler.tags = ['owner']
+handler.command = ['update', 'fix']
+handler.rowner = true
+
+export default handler

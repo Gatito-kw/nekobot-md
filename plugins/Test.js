ac6a908fdafa2d72bee89747fea077b335736dc2
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-02-26 14:46:34 -0500

    Add files via upload

---
+++ plugins/Test.js
@@ -0,0 +1,11 @@
+let handler = async (m, { conn, text }) => {
+   await conn.sendUrl(m.chat, 'Test', contextInfo: { mentionedJid: [m.sender], externalAdReply: { mediaType: 1, renderLargerThumbnail: true, sourceUrl: 'https://chat.whatsapp.com/NEKO', thumbnail: global.imgbot.neko3, thumbnailUrl: global.imgbot.neko3, title: '-' }})
+}
+
+handler.help = ['test']
+handler.tags = ['owner']
+handler.command = ['test']
+
+handler.rowner = true
+
+export default handler

43d2c5d1493a1d7854a9757915fd7e3408eededb
SinNombre999 <100889859+SinNombre999@users.noreply.github.com>
2023-02-26 14:47:13 -0600

    Update google.js

--- plugins/google.js
+++ plugins/google.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command, args }) => {
     const fetch = (await import('node-fetch')).default
     let full = /f$/i.test(command)
     let text = args.join` `
-    if (!text) return conn.reply(m.chat, 'Tidak ada teks untuk di cari', m)
+    if (!text) return conn.reply(m.chat, '❄️ Que desea buscar en Google?', m)
     let url = 'https://google.com/search?q=' + encodeURIComponent(text)
     let search = await googleIt(text)
     let msg = search.articles.map(({
@@ -19,12 +19,12 @@ let handler = async (m, { conn, command, args }) => {
         if (/<!DOCTYPE html>/i.test(ss.toBuffer().toString())) throw ''
         await conn.sendFile(m.chat, ss, 'screenshot.png', url + '\n\n' + msg, m)
     } catch (e) {
-        m.reply(msg)
-    }
+        conn.sendMessage(m.chat, {video: {url: "https://telegra.ph/file/fdbd14f129d8611c8a020.mp4"}, caption: '\t\t\t\t\t\t*乂 Google search 乂*\n\n' + msg, gifPlayback: true}, {quoted: m})
+   }
 }
-handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
+handler.help = ['google <consulta>']
 handler.tags = ['internet']
-handler.command = /^googlef?$/i
+handler.command = ['google'] 
 
 
 export default handler

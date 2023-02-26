0c66642a560d407ffcd5be0fa685af73000837f1
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-02-26 09:11:20 -0500

    Add files via upload

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,21 +1,30 @@
-import { mediafiredl } from '@bochilteam/scraper'
+import axios from 'axios'
+import cheerio from 'cheerio'
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
-    let res = await mediafiredl(args[0])
-    let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-    let caption = `
-*Name:* ${filename}
-*Size:* ${filesizeH}
-*Extension:* ${ext}
-*Uploaded:* ${aploud}
-`.trim()
-    m.reply(caption)
-    await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
+   if (!args[0]) return m.reply(`Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`)
+   let res = await mediafiredl(args[0])
+   await m.reply(`${JSON.stringify(res, null, 1)}`)
 }
-handler.help = ['mediafire'].map(v => v + ' <url>')
+
+handler.help = ['mediafire']
 handler.tags = ['downloader']
-handler.command = /^(mediafire|mf)$/i
+handler.command = ['mediafire', 'mdfire']
 
 handler.limit = true
 
 export default handler
+
+
+async function mediafireDl(url) {
+   const res = await axios.get(`https://www-mediafire-com.translate.goog/${url.replace('https://www.mediafire.com/','')}?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp`)
+   const $ = cheerio.load(res.data)
+   const link = $('#downloadButton').attr('href')
+   const name = $('body > main > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').attr('title').replaceAll(' ','').replaceAll('\n','')
+   const date = $('body > main > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text()
+   const size = $('#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '').replaceAll(' ','')
+   let mime = ''
+   let rese = await axios.head(link)
+   mime = rese.headers['content-type']
+   return { name, size, date, mime, link }
+}

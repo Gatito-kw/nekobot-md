991fafbc480343185849d69bf9f035c258b84838
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-02-25 19:51:12 -0500

    Add files via upload

--- plugins/tool-readviewonce.js
+++ plugins/tool-readviewonce.js
@@ -1,15 +1,12 @@
-let { downloadContentFromMessage } = (await import('@adiwajshing/baileys'));
-
 let handler = async (m, { conn }) => {
-    if (!m.quoted) throw 'where\'s message?'
-    if (m.quoted.mtype !== 'viewOnceMessage') throw 'Itu bukan pesan viewOnce'
-    const buffer = await m.quoted.download()
-    const media = m.quoted.mediaMessage[m.quoted.mediaType]
-    conn.sendFile(m.chat, buffer, /video/.test(media.mimetype) ? 'video.mp4' : 'image.jpg', media.caption || '', m)
+   if (!m.quoted) return m.reply('*Responde a un mensaje ViewOnce.*')
+   if (m.quoted.mtype !== 'viewOnceMessageV2') return m.reply('*Responde a un mensaje ViewOnce.*')
+   let media = await m.quoted.download()
+   await conn.sendFile(m.chat, media, 'Read-View-Once.jpg', m.quoted.text, m)
 }
 
 handler.help = ['readviewonce']
-handler.tags = ['tools']
-handler.command = /^readviewonce/i
+handler.tags = ['tool']
+handler.command = ['readviewonce', 'read', 'ver', 'readvo', 'rvo']
 
 export default handler

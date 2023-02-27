a13cf4bad42b8a23a3143a6975f6396d82ca3985
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-02-27 15:27:27 -0500

    Add files via upload

---
+++ plugins/group-welcome.js
@@ -0,0 +1,28 @@
+import db from '../lib/database.js'
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+   let chat = db.data.chats[m.chat] || {}
+   let rows = [
+      ['Activar', `${usedPrefix + command} on`],
+      ['Desactivar', `${usedPrefix + command} off`]
+   ]
+   if (args[0] === 'on') {
+      if (chat.welcome) return m.reply('*La bienvenida ya esta Activado.*')
+      chat.welcome = true
+      await m.reply('🚩 *Bienvenida activada para este Grupo.*')
+   } else if (args[0] === 'off') {
+      if (!chat.welcome) return m.reply('*La bienvenida ya esta Desactivado.*')
+      chat.welcome = false
+      await m.reply('🚩 *Bienvenida desactivada para este Grupo.*')
+   } else {
+      await conn.sendList(m.chat, null, `*Configuración de la Bienvenida. 🍟*\n\n	◦  *Estado* : [ ${chat.welcome ? 'ON' : 'OFF'} ]`, null, 'Tap!', [['🚩 Bienvenida.', rows]], m)
+   }
+}
+
+handler.help = ['welcome']
+handler.tags = ['group']
+handler.command = ['welcome', 'bienvenida']
+
+handler.admin = true
+
+export default handler

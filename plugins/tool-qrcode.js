2861024904babdad1d4fa94b5ec9d519513c1e8f
SinNombre999 <100889859+SinNombre999@users.noreply.github.com>
2023-02-27 10:29:55 -0600

    Update tool-qrcode.js

--- plugins/tool-qrcode.js
+++ plugins/tool-qrcode.js
@@ -1,8 +1,10 @@
 
 import { toDataURL } from 'qrcode'
 
-let handler = async (m, { conn, text }) => conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
-
+let handler = async (m, { conn, text }) => {
+if (!text) return m.reply('*🚩 Ingresa el texto que desees convertir en un código QR*') 
+conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
+}
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
 handler.tags = ['tools']
 handler.command = /^qr(code)?$/i

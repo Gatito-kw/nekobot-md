6f4a28360a1b58250f7315517b75c40119582e2a
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-02 23:42:53 -0500

    Add files via upload

--- plugins/group-welcome.js
+++ plugins/group-welcome.js
@@ -7,21 +7,22 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       ['Desactivar', `${usedPrefix + command} off`]
    ]
    if (args[0] === 'on') {
-      if (chat.welcome) return m.reply('*La bienvenida ya esta Activado.*')
+      if (chat.welcome) return m.reply('La bienvenida ya esta Activado.')
       chat.welcome = true
-      await m.reply('🚩 *Bienvenida activada para este Grupo.*')
+      await m.reply('🚩 Bienvenida activada para este Grupo.')
    } else if (args[0] === 'off') {
-      if (!chat.welcome) return m.reply('*La bienvenida ya esta Desactivado.*')
+      if (!chat.welcome) return m.reply('La bienvenida ya esta Desactivado.')
       chat.welcome = false
-      await m.reply('🚩 *Bienvenida desactivada para este Grupo.*')
+      await m.reply('🚩 Bienvenida desactivada para este Grupo.')
    } else {
-      await conn.sendList(m.chat, null, `*Configuración de la Bienvenida. 🍟*\n\n	◦  *Estado* : [ ${chat.welcome ? 'ON' : 'OFF'} ]`, null, 'Tap!', [['🚩 Bienvenida.', rows]], m)
+      await conn.sendList(m.chat, null, `*Configurar Bienvenida. 🍟*\n\n	◦  *Estado* : [ ${chat.welcome ? 'ON' : 'OFF'} ]`, null, 'Tap!', [['CONFIGURAR BIENVENIDA', rows]], m)
    }
 }
 
 handler.help = ['welcome']
 handler.tags = ['group']
 handler.command = ['welcome', 'bienvenida']
+handler.use = ['on / off']
 
 handler.admin = true
 

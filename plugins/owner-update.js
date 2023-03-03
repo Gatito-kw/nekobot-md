e4b7526f6a0bb143c6e2f04b1d321277c272d90e
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-03 01:48:49 -0500

    Add files via upload

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -7,15 +7,10 @@ let handler = async (m, { conn, text }) => {
    let u
    try {
       u = await exec('git pull')
-      await m.reply(`${JSON.stringify(u, null, 1)}`)
    } catch (e) {
       u = e
    } finally {
-      let { stdout, stderr } = u
-      let log
-      if (stderr.trim()) log += stderr.trim() + '\n\n\n'
-      if (stdout.trim()) log += stdout.trim()
-      await m.reply(log.trim())
+      await m.reply(JSON.stringify(u, null, 1))
    }
 }
 

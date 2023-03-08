2e7d25232a765aa79e6b5d930668add9a04dac5b
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-07 23:28:23 -0500

    Add files via upload

--- plugins/search-youtube.js
+++ plugins/search-youtube.js
@@ -1,21 +1,31 @@
 import yts from 'yt-search'
 
 let handler = async (m, {conn, usedPrefix, text }) => {
-   if (!text) return m.reply('*Ingrese el título de un video de YouTube*')
+   if (!text) return m.reply('Ingrese el título de un video de YouTube.')
    await m.react('🕓')
    let results = await yts(text)
    let res = results.all.map(v => v).filter(v => v.type == "video")
-   if (!res.length) return m.reply('*No se encontraron resultados, intente con un nombre más corto*').then(_ => m.react('✖️'))
-   let txt = `乂  *Y T  -  S E A R C H*\n`
+   if (!res.length) return m.reply('No se encontraron resultados, intente con un nombre más Corto.').then(_ => m.react('✖️'))
+   let txt = `乂  *Y T  -  S E A R C H*`
    for (let i = 0; i < 15; i++) {
-      txt += `\n`
+      txt += `\n\n`
       txt += `	◦  *Nro* : ${1+i}\n`
 	  txt += `	◦  *Titulo* : ${res[i].title}\n`
 	  txt += `	◦  *Autor* : ${res[i].author.name || '×'}\n`
 	  txt += `	◦  *Url* : ${'https://youtu.be/' + res[i].videoId}\n`
-	}
-    await conn.sendFile(m.chat, res[0].image, 'YouTube - Search.jpg', txt, m)
-    await m.react('✅')
+   }
+   let img = await (await fetch(res[0].image)).buffer()
+   await conn.sendUrl(m.chat, txt, m, {
+      mentionedJid: [m.sender],
+      externalAdReply: {
+         mediaType: 1,
+         renderLargerThumbnail: true,
+         thumbnail: img,
+         thumbnailUrl: img,
+         title: global.textbot.title,
+      }
+   })
+   await m.react('✅')
 }
 
 handler.help = ['ytsearch']

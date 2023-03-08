77d502fa059247417839b49ff218f1c660fdf8e4
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-08 11:06:38 -0500

    Add files via upload

---
+++ plugins/search-lyrics.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+   let res = await fetch('https://api.popcat.xyz/lyrics?song=' + text)
+   if (!res.ok) return m.react('❌')
+   let json = await res.json()
+   if (!json.lyrics) return m.react('❌')
+   let txt = `*Lyrics Search*\n\n`
+      txt += `*Titulo* : ${json.title}\n`
+      txt += `*Artista* : ${json.title}\n\n`
+      txt += `*Letra*\n${json.title}`
+   await m.reply(txt)
+}
+
+handler.help = ['lyrics']
+handler.tags = ['search']
+handler.command = ['lyrics', 'lyric']
+
+export default handler

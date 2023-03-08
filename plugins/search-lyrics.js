4c6138f396c85f69fe6f192b786eff384b385cd2
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-08 11:25:51 -0500

    Add files via upload

--- plugins/search-lyrics.js
+++ plugins/search-lyrics.js
@@ -5,11 +5,20 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
    if (!res.ok) return m.react('❌')
    let json = await res.json()
    if (!json.lyrics) return m.reply('Letra no encontrada, intenta con otro Titulo.').then(async _ => await m.react('✖️'))
-   let txt = `*Lyrics Search*\n\n`
-      txt += `*Titulo* : ${json.title}\n`
-      txt += `*Artista* : ${json.artist}\n\n`
-      txt += `*Letra*\n${json.lyrics}`
-   await m.reply(txt)
+   let txt = `乂  *L Y R I C S  -  S E A R C H*\n\n`
+      txt += `	◦  *Titulo* : ${json.title}\n`
+      txt += `	◦  *Artista* : ${json.artist}\n\n`
+      txt += `${json.lyrics}`
+   let img = await (await fetch(json.image)).buffer()
+   await conn.sendUrl(m.chat, txt, m, {
+      externalAdReply: {
+         mediaType: 1,
+         renderLargerThumbnail: true,
+         thumbnail: img,
+         thumbnailUrl: img,
+         title: global.textbot.title,
+      }
+   })
 }
 
 handler.help = ['lyrics']

ceca45c83859057482ca871e0ba86703d8a297ce
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-07 23:51:14 -0500

    Add files via upload

---
+++ plugins/fun-chatgpt.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch'
+
+let handler = async(m, { conn, text }) => {
+    if (!text) return m.reply('Ingresa un texto para interactuar con la inteligencia artificial de Chatgpt.')
+    let res = await openAi(text)
+    await m.reply(`${res?.choices[0]?.text.trim()}`).catch(e => m.reply(JSON.stringify(res, null, 1)))
+}
+
+handler.help = ['chatgpt']
+handler.tags = ['fun']
+handler.command = ['chatgpt']
+
+export default handler
+
+async function openAi(text) {
+    let result = await fetch('https://api.openai.com/v1/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer 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'
+        },
+        body: JSON.stringify({
+            'model': 'text-davinci-003',
+            'prompt': text,
+            'temperature': 0.5,
+            'max_tokens': 3000,
+            'top_p': 1,
+        })
+    })
+    return await result.json()
+}

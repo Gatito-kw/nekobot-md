43dec9dc11bcb12e137affdc7b59aecf826c5695
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-08 17:14:02 -0500

    Add files via upload

--- plugins/fun-chatgpt.js
+++ plugins/fun-chatgpt.js
@@ -1,22 +1,9 @@
-import { Configuration, OpenAIApi } from 'openai'
-
-const configuration = new Configuration({
-   apiKey: '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',
-})
-const openai = new OpenAIApi(configuration)
+import axios from 'axios'
 
 let handler = async(m, { conn, text }) => {
    if (!text) return m.reply('Ingresa un texto para interactuar con la inteligencia artificial de Chatgpt.')
-   const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: text,
-      temperature: 0,
-      max_tokens: 2048,
-      top_p: 1,
-      frequency_penalty: 0.3,
-      presence_penalty: 0
-   })
-   await m.reply(`${response.data.choices[0].text}`)
+   let res = await openAi(text)
+   await m.reply(`${JSON.stringify(anu, null, 1)}`)
 }
 
 handler.help = ['chatgpt']
@@ -24,3 +11,23 @@ handler.tags = ['fun']
 handler.command = ['chatgpt']
 
 export default handler
+
+async function openAi(text) {
+   const { data, status } = await axios.post('https://api.openai.com/v1/completions', {
+      text,
+      model: 'text-davinci-003',
+      max_tokens: 500,
+      temperature: 0,
+      stream: false
+   }, {
+      headers: {
+         'Content-Type': 'application/json',
+         Authorization: 'Bearer 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'
+      }
+   })
+   if (status !== 200) throw new Error('Opps error!')
+   return data
+  } catch (err) {
+   return err.message
+  }
+}

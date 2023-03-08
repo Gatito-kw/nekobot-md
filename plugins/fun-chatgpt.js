79ca2ac0561ecbb2c3a5d3df584eb2a59665f6d4
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-08 17:00:34 -0500

    Add files via upload

--- plugins/fun-chatgpt.js
+++ plugins/fun-chatgpt.js
@@ -1,15 +1,22 @@
 import { Configuration, OpenAIApi } from 'openai'
 
 const configuration = new Configuration({
-   organization: 'org-pHcQAj5wjFIpcdkfmpFePVqF',
    apiKey: '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',
 })
 const openai = new OpenAIApi(configuration)
 
 let handler = async(m, { conn, text }) => {
    if (!text) return m.reply('Ingresa un texto para interactuar con la inteligencia artificial de Chatgpt.')
-   const res = await openai.createCompletion({ model: "text-davinci-003", prompt: text, temperature: 0, max_tokens: 3000, stop: ["Ai:", "Human:"], top_p: 1, frequency_penalty: 0.2, presence_penalty: 0 })
-   await m.reply(res.data.choices[0].text.trim())
+   const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: text,
+      temperature: 0,
+      max_tokens: 2048,
+      top_p: 1,
+      frequency_penalty: 0.3,
+      presence_penalty: 0
+   })
+   await m.reply(`${response.data.choices[0].text}`)
 }
 
 handler.help = ['chatgpt']

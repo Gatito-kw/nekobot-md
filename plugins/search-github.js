4583acca87fdbb23e2d1fa12eb3f56c4f95f951a
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-08 21:51:46 -0500

    Add files via upload

--- plugins/search-github.js
+++ plugins/search-github.js
@@ -12,15 +12,13 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
    for (let i = 0; i < (15 <= result.length ? 15 : result.length); i++) {
       txt += `\n\n`
       txt += `	◦  *Nro* : ${1+i}\n`
+      txt += `	◦  *Url* : ${result[i].html_url}\n`
 	  txt += `	◦  *Nombre* : ${result[i].name}\n`
 	  txt += `	◦  *Creador* : ${result[i].owner.login}\n`
 	  txt += `	◦  *Creado* : ${moment(result[i].created_at).format('DD/MM/YY - HH:mm:ss')}\n`
 	  txt += `	◦  *Actualizado* : ${moment(result[i].updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
 	  txt += `	◦  *Visitas* : ${result[i].watchers}\n`
-	  txt += `	◦  *Forks* : ${result[i].forks}\n`
-	  txt += `	◦  *Stars* : ${result[i].stargazers_count}\n`
-	  txt += `	◦  *Issues* : ${result[i].open_issues}\n`
-	  txt += `	◦  *Url* : ${result[i].html_url}\n`
+	  txt += `	${result[i].forks} Forks · ${result[i].stargazers_count} Stars · ${result[i].open_issues} Issues\n`
    }
    let img = await (await fetch(json.items[0].owner.avatar_url)).buffer()
    await conn.sendUrl(m.chat, txt, m, {

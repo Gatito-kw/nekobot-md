118f6e894fb6bd3fa2d1da25f455f11320fabe32
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-08 22:58:32 -0500

    Add files via upload

--- plugins/image-wallpaper.js
+++ plugins/image-wallpaper.js
@@ -1,13 +1,20 @@
-import { wallpaper, wallpaperv2 } from '@bochilteam/scraper'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
-    const res = await (/2/.test(command) ? wallpaperv2 : wallpaper)(text)
-    const img = res[Math.floor(Math.random() * res.length)]
-    conn.sendFile(m.chat, img, 'wallpaper.jpg', `Result from *${text}*`, m)
-}
-handler.help = ['', '2'].map(v => 'wallpaper' + v + ' <query>')
-handler.tags = ['downloader']
+import fetch from 'node-fetch'
 
-handler.command = /^(wallpaper2?)$/i
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+   if (!text) return m.reply('Ingresa el titulo de un Wallpaper.')
+   await m.react('🕓')
+   let res = await fetch(global.API('https://wall.alphacoders.com/api2.0','/get.php', { auth: '3e7756c85df54b78f934a284c11abe4e', method: 'search', term: text }))
+   if (!res.ok) return m.react('❌')
+   let json = await res.json()
+   let img = json.wallpapers[Math.floor(Math.random() * json.wallpapers.length)]
+   await conn.sendButton(m.chat, '*WALLPAPER*', 'Tap!', img.url_image, [['Siguiente ➡️', `${usedPrefix + command}`]], m)
+   await m.react('✅')
+}
+
+handler.help = ['wallpaper']
+handler.tags = ['img']
+handler.command = ['wallpaper']
+
+handler.react_error = true
 
 export default handler

fc4243b5323e66565380044a61d209d7a532a3ac
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-08 23:01:34 -0500

    Add files via upload

--- plugins/fun-shipping.js
+++ plugins/fun-shipping.js
@@ -1,12 +1,12 @@
 let toM = a => '@' + a.split('@')[0]
 
-function handler(m, { groupMetadata }) {
-    let ps = groupMetadata.participants.map(v => v.id)
-    let a = ps.getRandom()
-    let b
-    do b = ps.getRandom()
-    while (b === a)
-    await m.reply(`${toM(a)} ❤️ ${toM(b)}`, null, { mentions: [a, b] })
+let handler = async (m, { groupMetadata }) => {
+   let ps = groupMetadata.participants.map(v => v.id)
+   let a = ps.getRandom()
+   let b
+   do b = ps.getRandom()
+   while (b === a)
+   await m.reply(`${toM(a)} ❤️ ${toM(b)}`, null, { mentions: [a, b] })
 }
 
 handler.help = ['shipping']

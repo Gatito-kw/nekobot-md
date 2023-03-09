c8fb58a7bf61565e80dba1a5cd02235995308296
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-08 20:17:46 -0500

    Add files via upload

--- plugins/search-youtube.js
+++ plugins/search-youtube.js
@@ -8,7 +8,7 @@ let handler = async (m, {conn, usedPrefix, text }) => {
    let res = results.all.map(v => v).filter(v => v.type == "video")
    if (!res.length) return m.reply('No se encontraron resultados, intente con un nombre más Corto.').then(_ => m.react('✖️'))
    let txt = `乂  *Y T  -  S E A R C H*`
-   for (let i = 0; i < (10 <= res.length ? 10 : res.length); i++) {
+   for (let i = 0; i < (15 <= res.length ? 15 : res.length); i++) {
       txt += `\n\n`
       txt += `	◦  *Nro* : ${1+i}\n`
 	  txt += `	◦  *Titulo* : ${res[i].title}\n`

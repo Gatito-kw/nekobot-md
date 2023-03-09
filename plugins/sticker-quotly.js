3619cb14edcadc1c4aaf41a6925da70ed0a78fbb
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-09 01:38:06 -0500

    Add files via upload

---
+++ plugins/sticker-quotly.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+let handler = async (m, { conn, text }) => {
+   if (!text) return m.reply('Ingresa un Texto.')
+   if (text.length > 30) return m.reply('Solo se permiten 30 caracteres como Máximo.')
+   let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => global.imgbot.noprofile)
+   const obj = {
+      "type": "quote",
+      "format": "png",
+      "backgroundColor": "#FFFFFF",
+      "width": 512,
+      "height": 768,
+      "scale": 2,
+      "messages": [{
+         "entities": [],
+         "avatar": true,
+         "from": {
+            "id": 1,
+            "name": m.name,
+            "photo": {
+               "url": pp
+            }
+         },
+         "text": text,
+         "replyMessage": {}
+      }]
+   }
+   const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {
+      headers: {
+         'Content-Type': 'application/json'
+      }
+   })
+   await m.reply(`${JSON.stringify(json, null, 1)}`)
+}
+
+handler.help = ['quotly']
+handler.tags = ['sticker']
+handler.command = ['quotly']
+
+export default handler

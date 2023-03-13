867bf33f4c335da2d3f40c3045fb2f04cb00eb73
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-13 17:24:26 -0500

    Add files via upload

---
+++ plugins/tools/setppgroup.js
@@ -0,0 +1,47 @@
+import Jimp from 'jimp'
+
+let handler = async (m, { conn, command, usedPrefix }) => {
+   let q = m.quoted ? m.quoted : m
+   let mime = (q.msg || q).mimetype || q.mediaType || ''
+   if (/image/g.test(mime)) return m.reply('Responde a una Imagen.')
+   let media = await q.download()
+   let { img } = await pepe(media)
+   if (args[0] == '--full') {
+      let { img } = await pepe(media)
+      await conn.query({
+         tag: 'iq',
+         attrs: {
+            to: conn.user.jid,
+            type: 'set',
+            xmlns: 'w:profile:picture'
+         },
+         content: [{
+            tag: 'picture',
+            attrs: { type: 'image' },
+            content: img
+         }]
+      })
+      return !0
+   }
+   await conn.updateProfilePicture(m.chat, media)
+   await m.reply('🚩 Perfil del grupo cambiando con Exito.')
+}
+
+handler.help = ['setppgroup']
+handler.tags = ['tool']
+handler.command = ['setppgroup', 'setppgp']
+
+handler.owner = true
+
+export default handler
+
+async function pepe(media) {
+   const jimp_1 = await Jimp.read(media)
+   const min = jimp_1.getWidth()
+   const max = jimp_1.getHeight()
+   const cropped = jimp_1.crop(0, 0, min, max)
+   return {
+      img: await cropped.scaleToFit(720, 720).getBufferAsync(Jimp.MIME_JPEG),
+      preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG)
+   }
+}

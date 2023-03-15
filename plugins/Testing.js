04728bd9415360bddde7e6893c15f234e5184526
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-15 14:37:21 -0500

    Update Testing.js

--- plugins/Testing.js
+++ plugins/Testing.js
@@ -2,8 +2,8 @@ import { plugins } from '../lib/plugins.js'
 
 let handler = async (m, { conn, args, text }) => {
    let cmd = Object.values(plugins).filter((v) => v.tags && v.help.find((x) => x == args[0]))
-   console.log(cmd[0])
-   await m.reply(`${JSON.stringify(cmd[0], null, 1)}`)
+   console.log(cmd[0].command)
+   await m.reply(`${JSON.stringify(cmd[0].command, null, 1)}`)
 }
 
 handler.command = ['test']

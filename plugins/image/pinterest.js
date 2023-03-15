bde62267fc81db6e0454528a92068665b42a9b1f
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-15 16:50:51 -0500

    Add files via upload

--- plugins/image/pinterest.js
+++ plugins/image/pinterest.js
@@ -4,8 +4,11 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
    if (!text) return m.reply('Ingresa que imagen deseas buscar en Pinterest.')
    await m.react('🕓')
-   let img = await pinterest(text)
-   if (!img.url) return m.react('❌')
+   let res = await fetch(`https://www.pinterest.com/resource/BaseSearchResource/get/?source_url=%2Fsearch%2Fpins%2F%3Fq%3D${text}&data=%7B%22options%22%3A%7B%22isPrefetch%22%3Afalse%2C%22query%22%3A%22${text}%22%2C%22scope%22%3A%22pins%22%2C%22no_fetch_context_on_resource%22%3Afalse%7D%2C%22context%22%3A%7B%7D%7D&_=1619980301559`)
+   let json = await res.json()
+   let data = json?.resource_response?.data?.results
+   let img = data[~~(Math.random() * (data?.length))]?.images?.orig
+   if (!img) return m.reply('No se encontró la imagen, intenta con otro Nombre.').then(async _ => await m.react('✖️'))
    await conn.sendButton(m.chat, `*PINTEREST*`, 'Click para la siguiente Imagen', img.url, [['Siguiente ➡️', `${usedPrefix + command} ${text}`]], m)
    await m.react('✅')
 }
@@ -17,10 +20,3 @@ handler.command = ['pinterest']
 handler.react_error = true
 
 export default handler
-
-async function pinterest(query) {
-   let res = await fetch(`https://www.pinterest.com/resource/BaseSearchResource/get/?source_url=%2Fsearch%2Fpins%2F%3Fq%3D${query}&data=%7B%22options%22%3A%7B%22isPrefetch%22%3Afalse%2C%22query%22%3A%22${query}%22%2C%22scope%22%3A%22pins%22%2C%22no_fetch_context_on_resource%22%3Afalse%7D%2C%22context%22%3A%7B%7D%7D&_=1619980301559`)
-   let json = await res.json()
-   let data = json.resource_response.data.results
-   return data[~~(Math.random() * (data.length))].images.orig
-}
--- plugins/image/wallpaper.js
+++ plugins/image/wallpaper.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let res = await fetch(`https://api.lolhuman.xyz/api/wallpaper?apikey=BrunoSobrino&query=${text}`)
     if (!res.ok) return m.react('❌')
     let img = await res.buffer()
-    if (img == undefined) return m.reply('No se encontró el wallpaper, intenta con otro nombre').then(async _ => await m.react('✖️'))
+    if (img == undefined) return m.reply('No se encontró el wallpaper, intenta con otro Nombre.').then(async _ => await m.react('✖️'))
     await conn.sendButton(m.chat, '*WALLPAPER*', 'Click para la siguiente Imagen', img, [['Siguiente ➡️', `${usedPrefix + command} ${text}`]], m)
     await m.react('✅️')
 }

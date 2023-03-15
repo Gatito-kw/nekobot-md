9b119e6c268ece24702966e0baaef203d998d2c4
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-15 17:37:08 -0500

    Delete Testing.js

--- plugins/Testing.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, text }) => {
-   let res = await Testing(text)
-   await m.reply(`${JSON.stringify(res, null, 1)}`)
-}
-
-handler.command = ['testing']
-
-export default handler

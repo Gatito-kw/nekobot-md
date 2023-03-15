6fb1b0dce0caae1d30ea8f63c2aac133ad76b607
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-15 14:24:12 -0500

    Update Testing.js

--- plugins/Testing.js
+++ plugins/Testing.js
@@ -1,7 +1,7 @@
 import { plugins } from '../lib/plugins.js'
 
 let handler = async (m, { conn, text }) => {
-   let cmd = Object.values(plugins).filter((v) => v.help && v.tags).find((c) => c)
+   let cmd = Object.values(plugins).filter((v) => v.help && v.tags).find(c => c)
    console.log(cmd)
 }
 

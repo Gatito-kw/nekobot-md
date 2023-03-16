16fd2372681781dd191fda1395b34fc1cdf72bb2
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-16 13:09:53 -0500

    Add files via upload

--- plugins/tools/linkpoi.js
+++ plugins/tools/linkpoi.js
@@ -2,6 +2,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
    if (!args[0]) return m.reply('Ingresa el enlace que quieres Acortar.')
+   if (!isURL(args[0])) return m.reply('Ingresa una url Válida.')
    let res = await fetch('https://linkpoi.ga/api.php?url=' + args[0])
    let json = await res.json()
    await m.reply(json.shorturl.replace('\/','/'))
@@ -12,3 +13,8 @@ handler.tags = ['tool']
 handler.command = ['linkpoi']
 
 export default handler
+
+function isURL(url) {
+   var regex = /^(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/
+   return regex.test(url)
+}

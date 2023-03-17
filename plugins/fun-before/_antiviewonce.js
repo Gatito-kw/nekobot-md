6c02acb52866d4cfd8e4506b60317de41d3fa311
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-16 23:25:41 -0500

    Add files via upload

--- plugins/fun-before/_antiviewonce.js
+++ plugins/fun-before/_antiviewonce.js
@@ -1,9 +1,9 @@
 import db from '../../lib/database.js'
 
 export async function before(m, { conn }) {
-   let chat = db.data.chats[m.chat]
    if (!m.isGroup)
       return !1
+   let chat = db.data.chats[m.chat]
    if (chat.antiViewOnce && m.mtype == 'viewOnceMessageV2') {
       let media = await m.download()
       await conn.sendFile(m.chat, media, 'Anti-View-Once.jpg', m.text, m)

00a338b9713c10a47b374fa0e1d619a9fcce9b90
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-17 00:09:25 -0500

    Add files via upload

--- plugins/info/script.js
+++ plugins/info/script.js
@@ -5,12 +5,12 @@ let handler = async (m, { conn, args }) => {
    let res = await fetch('https://api.github.com/repos/gatito-kw/nekobot-md')
    let json = await res.json()
    let txt = `*乂  B O T  -  S C R I P T*\n\n`
-      txt += `	◦  *Url* : ${json.html_url}\n\n`
+      txt += `	◦  *Url* : ${json.html_url}\n`
       txt += `	◦  *Nombre* : ${json.name}\n`
       txt += `	◦  *Visitas* : ${json.watchers_count}\n`
       txt += `	◦  *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
       txt += `	◦  *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
-      txt += `	${json.forks_count} Forks · ${json.stargazers_count} Stars · ${json.open_issues_count} Issues\n`
+      txt += `	${json.forks_count} Forks · ${json.stargazers_count} Stars · ${json.open_issues_count} Issues\n\n`
       txt += global.textbot.footer
    await conn.sendUrl(m.chat, txt, m, {
       externalAdReply: {

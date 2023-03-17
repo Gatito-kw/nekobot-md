208cd2ba3c645d9faae09398c3899195898be455
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-17 16:32:43 -0500

    Add files via upload

--- plugins/group/group.js
+++ plugins/group/group.js
@@ -8,11 +8,11 @@ let handler = async (m, { conn, args, usedPrefix, command, groupMetadata }) => {
    ]
    if (args[0] === 'open') {
       if (!group.announce) return m.reply('El grupo ya está Abierto.')
-      await conn.groupSettingUpdate(m.chat, isClose)
+      await conn.groupSettingUpdate(m.chat, 'not_announcement')
       await m.reply('🚩 Grupo abierto con Exito.')
    } else if (args[0] === 'close') {
       if (group.announce) return m.reply('El grupo ya está Cerrado.')
-      await conn.groupSettingUpdate(m.chat, 'not_announcement')
+      await conn.groupSettingUpdate(m.chat, 'announcement')
       await m.reply('🚩 Grupo cerrado con Exito.')
    } else {
       await conn.sendList(m.chat, null, `*Configurar Grupo. 🍟*\n\n	◦  *Estado* : [ ${!group.announce ? 'OPEN' : 'CLOSE'} ]`, null, 'Tap!', [['CONFIGURAR GRUPO', rows]], m)

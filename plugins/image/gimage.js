31db37785bbf12f8e2206bc129000056faf71ac6
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-19 22:00:39 -0500

    Add files via upload

--- plugins/image/gimage.js
+++ plugins/image/gimage.js
@@ -1,10 +1,9 @@
-import gis from 'g-i-s'
+import gis from 'async-g-i-s'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
    if (!text) return m.reply('Ingresa que imagen deseas buscar en Google.')
    await m.react('🕓')
    let res = await gis(text)
-   return console.log(res)
    if (!res.length) return m.reply('Imagen no encontrada, Intenta con otro Nombre.').then(async _ => await m.react('✖️'))
    await conn.sendButton(m.chat, '*IMAGEN*', 'Click para la siguiente Imagen', res.getRandom(), [['Siguiente ➡️', `${usedPrefix + command} ${text}`]], m)
    await m.react('✅️')
@@ -14,5 +13,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.help = ['gimage']
 handler.tags = ['image']
 handler.command = ['gimage', 'image']
+
+handler.react_error = true
 
 export default handler

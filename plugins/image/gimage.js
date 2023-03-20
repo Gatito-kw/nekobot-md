f7260c69ffb4a9bdb70a5430cb5f052c882aebe0
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-19 21:33:12 -0500

    Add files via upload

--- plugins/image/gimage.js
+++ plugins/image/gimage.js
@@ -1,9 +1,10 @@
-import { googleImage } from '@bochilteam/scraper'
+import gis from 'g-i-s'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
    if (!text) return m.reply('Ingresa que imagen deseas buscar en Google.')
    await m.react('🕓')
-   let res = await googleImage(text)
+   let res = await gis(text)
+   return console.log(res)
    if (!res.length) return m.reply('Imagen no encontrada, Intenta con otro Nombre.').then(async _ => await m.react('✖️'))
    await conn.sendButton(m.chat, '*IMAGEN*', 'Click para la siguiente Imagen', res.getRandom(), [['Siguiente ➡️', `${usedPrefix + command} ${text}`]], m)
    await m.react('✅️')

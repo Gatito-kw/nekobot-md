db7641dc1e6585b59ae30e26a4fc9744301d7396
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-21 20:27:55 -0500

    Add files via upload

--- plugins/group/tagall.js
+++ plugins/group/tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants }) => {
    let users = participants.map(u => conn.decodeJid(u.id))
-   await m.reply(`*乂  T A G A L L*${text ? '\n\n' + text : ''}\n\n${member.map(v => '◦  @' + v.replace(/@.+/, '')).join('\n')}`)
+   await m.reply(`*乂  T A G A L L*${text ? '\n\n' + text : ''}\n\n${users.map(v => '◦  @' + v.replace(/@.+/, '')).join('\n')}`)
 }
 
 handler.help = ['tagall']

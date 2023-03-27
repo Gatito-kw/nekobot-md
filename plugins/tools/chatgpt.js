3f7ca9e821bce522794a38a32d1e08b3aca96b73
Gatito - ネコ <84881966+Gatito-kw@users.noreply.github.com>
2023-03-27 17:41:04 -0500

    Add files via upload

--- plugins/tools/chatgpt.js
+++ plugins/tools/chatgpt.js
@@ -4,7 +4,7 @@ let handler = async (m, { text }) => {
    if (!text) return m.reply('Ingrese un Texto')
    let res = await fetch(`https://api.ibeng.tech/api/info/openai?text=${text}&apikey=tamvan`)
    let json = await res.json()
-   await m.reply(json.data.data.trim())
+   await m.reply(json.data.data ? json.data.data.trim() : json.data.message)
 }
 
 handler.help = ['chatgpt']
